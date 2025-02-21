3e1cf666299c6a9da46ef947e89b101d36abee9b
joseantbus <anubis007@gmail.com>
2025-02-21 19:59:12 -0400

    Add files via upload
    Primera version de la implementacion del challenge, falta refactorizar codigo

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista = [];
+
+function agregarAmigo() {
+   let amigo = document.getElementById('amigo').value;
+   if (amigo == '') {
+      alert('Por favor ingrese un nombre valido');
+   } else {
+      lista.push(amigo);
+      document.getElementById('amigo').value = '';
+      console.log(lista);
+   }
+   return;
+}
+
+function listaAmigos() {
+   let listaAmigo = document.getElementById('listaAmigos');
+   listaAmigo.innerHTML = '';
+   for (let i = 0; i < lista.length; i++) {
+      let amigos = lista[i];
+      let nuevoElemento = document.createElement('li');
+      nuevoElemento.textContent = `${amigos}`;
+      listaAmigo.appendChild(nuevoElemento);
+   }
+}
+
+function sortearAmigo() {
+   let amigoSorteado = document.getElementById('resultado');
+   if (lista.length === 0) {
+      alert('Debe introducir el nombre de sus amigos para realizar el sorteo');
+   } else {
+      let indiceAleatorio = Math.floor(Math.random() * lista.length);
+      let nuevoElemento = document.createElement('li');
+      nuevoElemento.textContent = `Su amigo secreto es ${lista[indiceAleatorio]}`;
+      amigoSorteado.appendChild(nuevoElemento);
+   }
+}
